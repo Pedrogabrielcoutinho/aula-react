12a80870155ccf9a6f63431f8441681ea3c80e96
Pedrogabrielcoutinho <pedrogabrielcoutinho099@mgmaill.com>
2024-05-21 17:16:19 -0300

    Adicionando um array de objetos que contem as imagens e textos do highlight da trybe, e depois chamamos esse array de objetos utilizando o map para exibir cada um, além de já colocar uma key para identificar, caso eu queira alterar no futuro

--- src/components/HighlightList.tsx
+++ src/components/HighlightList.tsx
@@ -1,11 +1,28 @@
 import Highlight from './Highlight';
 
 function HighlightList() {
+  const HighlightData = [
+    {
+      image: 'https://betrybe.github.io/images/trybe/chat.jpg',
+      text: 'Depoimento',
+    },
+    {
+      image: 'https://betrybe.github.io/images/trybe/midia.jpg',
+      text: 'Mídia',
+    },
+    {
+      image: 'https://betrybe.github.io/images/trybe/money.jpg',
+      text: 'Investimento',
+    },
+  ];
   return (
     <div className="highlight__container">
-      <Highlight />
-      <Highlight />
-      <Highlight />
+      {
+        HighlightData.map((item) => {
+          console.log(item.text);
+          return <Highlight key={ item.text } />;
+        })
+      }
     </div>
   );
 }
